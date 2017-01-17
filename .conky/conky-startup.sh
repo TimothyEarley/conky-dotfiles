e3557c2679f58fc8f68a00f53fbbecfcead24d0d
Timothy Earley <timmy@earley.de>
2017-01-17 17:13:36 +0100

    Added vision widget

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,11 @@
+sleep 2s
+killall conky
+
+cd "/home/timmy/.conky/now-clocking/np"
+conky -c "/home/timmy/.conky/now-clocking/np/np" &
+
+cd "/home/timmy/.conky/now-clocking/np"
+conky -c "/home/timmy/.conky/now-clocking/np/npart" &
+
+cd "/home/timmy/.conky/conky-Vision/"
+conky -c "/home/timmy/.conky/conky-Vision/.conkyrc" &
