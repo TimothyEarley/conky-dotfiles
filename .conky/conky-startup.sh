c2c6dba46c93e1e5e6744b5875bcdbd4b783c411
Timothy Earley <timmy@earley.de>
2018-06-04 23:20:12 +0200

    Updated now clocking, Adapted startup

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -4,8 +4,11 @@ killall conky
 cd "/home/timmy/.conky/now-clocking/np"
 conky -c "/home/timmy/.conky/now-clocking/np/np" &
 
-cd "/home/timmy/.conky/now-clocking/np"
-conky -c "/home/timmy/.conky/now-clocking/np/npart" &
+#cd "/home/timmy/.conky/now-clocking/np"
+#conky -c "/home/timmy/.conky/now-clocking/np/npart" &
 
-cd "/home/timmy/.conky/conky-Vision/"
-conky -c "/home/timmy/.conky/conky-Vision/.conkyrc" &
+#cd "/home/timmy/.conky/conky-Vision/"
+#conky -c "/home/timmy/.conky/conky-Vision/.conkyrc" &
+
+cd "/home/timmy/.conky/metro-clock/"
+conky -c "/home/timmy/.conky/metro-clock/.conkyrc" &
